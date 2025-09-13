525090de6a3414574b7930de656769428faac1d6
JakeRiggss <jakeriggs2015@gmail.com>
2025-09-13 05:20:52 +0000

    homework 2 finished

---
+++ hw2/hw2.py
@@ -0,0 +1,84 @@
+# 2.3
+grade = 91
+
+if grade >= 90:
+    print("Congratulations! Your grade of 91 earns you an A in this course")
+
+
+
+
+# 2.4
+x = 27.5 + 2
+print("addition: ", x)
+
+x = 27.5 - 2
+print("subtraction: ", x)
+
+x = 27.5 * 2
+print("multiplication: ", x)
+
+x = 27.5 / 2
+print("division: ", x)
+
+x = 27.5 // 2
+print("floor: ", x)
+
+x = 27.5 ** 2
+print("modulus operator: ", x)
+
+
+
+
+# 2.5
+radius = 2
+pi = 3.14159
+
+diameter = 2 * radius 
+circumference = 2 * pi * radius 
+area = pi * radius * radius 
+
+print("Diameter: ", diameter)
+print("circumference: ", circumference)
+print("area: ", area)
+
+
+
+
+# 2.6
+num = int(input("enter an integer: "))
+
+if num % 2 == 0:
+    print(num, "is even")
+
+if num % 2 != 0:
+    print(num, "is odd")
+
+
+
+
+# 2.7
+if 1024 % 4 == 0:
+    print("1024 is a multiple of 4")
+
+if 2 % 10 == 0:
+    print("2 is a multiple of 10")
+
+
+
+
+# 2.8
+print("number\tsquare\tcube")
+
+print(0, "\t", 0**2, "\t", 0**3)
+print(1, "\t", 1**2, "\t", 1**3)
+print(2, "\t", 2**2, "\t", 2**3)
+print(3, "\t", 3**2, "\t", 3**3)
+print(4, "\t", 4**2, "\t", 4**3)
+print(5, "\t", 5**2, "\t", 5**3)
+
+
+
+
+
+
+
